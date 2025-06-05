7ab6437eb1e61947640d1f3e68668e8b8d00c560
MorrisLin <aka611511@gmail.com>
2025-06-05 19:19:23 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import enums.ProductStatusEnum;
 import model.OrderDetail;
 import model.Orders;
+import model.Product;
 import service.OrderDetailService;
 import service.OrderService;
 import service.ProductService;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,32 +24,32 @@ public class Main {
             OrderDetail o = new OrderDetail(3, 3, 2);
             orderDetailService.insert(o);
             System.out.println(orderDetailService.exportCSV());
-//            Product product = new Product(120, "鴛鴦奶茶2", ProductStatusEnum.OutOfStock);
-//            productService.insert(product);
-//            System.out.println(product);
-//            System.out.println(ProductStatusEnum.getEnum("out-of-stock"));
-//            Product product1 = productService.get(4);
-//            System.out.println(productService.getSaleProducts());
-//            System.out.println(productService.insert(product));
-//            List<Product> productList = productService.getAll();
-//            System.out.println(productList);
-//            if (!productList.isEmpty()) {
-//                System.out.println(productList.get(2));
-//                Product product1 = productService.get(4);
-//                System.out.println(product1);
-//                product1.setPrice(200);
-//                product1.setName("香草拿鐵");
-//                boolean success = productService.update(product1);
-//                if (success) {
-//                    product1 = productService.get(4);
-//                    System.out.println(product1);
-//                }
-//            }
-//            System.out.println(productService.getProductByName("拿鐵"));
-//            boolean success = productService.delete(1);
-//            if (success) {
-//                System.out.println(productService.get(1));
-//            }
+            Product product = new Product(120, "鴛鴦奶茶2", ProductStatusEnum.OutOfStock);
+            productService.insert(product);
+            System.out.println(product);
+            System.out.println(ProductStatusEnum.getEnum("out-of-stock"));
+            Product product1 = productService.get(4);
+            System.out.println(productService.getSaleProducts());
+            System.out.println(productService.insert(product));
+            List<Product> productList = productService.getAll();
+            System.out.println(productList);
+            if (!productList.isEmpty()) {
+                System.out.println(productList.get(2));
+                Product product1 = productService.get(4);
+                System.out.println(product1);
+                product1.setPrice(200);
+                product1.setName("香草拿鐵");
+                boolean success = productService.update(product1);
+                if (success) {
+                    product1 = productService.get(4);
+                    System.out.println(product1);
+                }
+            }
+            System.out.println(productService.getProductByName("拿鐵"));
+            boolean success = productService.delete(1);
+            if (success) {
+                System.out.println(productService.get(1));
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
